1180b70c8284452829492eb04f8bee92ffe26ab4
Stephan Kern <stephan@swmmrman.com>
2019-09-13 09:42:34 -0600

    added error handling

--- simMonitor.py
+++ simMonitor.py
@@ -17,31 +17,42 @@ url = 'http://rs2.taggrid.org:9100/jsonSimStats'
 
 class SimWatcher:
     def main(self):
-        req = requests.get(url).json()
-        fps = req['SimFPS']
-        up = req["Uptime"]
-        upt = re.split(r":|\.", up)
-        agents = req['RootAg']
-        prims = req['Prims']
-        lps = req['ScrLPS']
-        (tagV, osV) = req['Version'].split('\n')
-        tagV = tagV.strip()
-        osV = osV.strip()
-        statsString = \
-            f"Sim FPS:\t\t {fps}\n"\
-            f"Visitors:\t\t {agents}\n"\
-            f"Prims: \t\t {prims}\n"\
-            f"Script LPS:\t\t {lps}\n"\
-            f"Tag Version:\t\t {tagV}\n"\
-            f"OpenSim Version:\t{osV}\n\n"\
-            f"Sim uptime:\t\t Days:{upt[0]}\n"\
-            f"\t\t Hours:{upt[1]}\n"\
-            f"\t\t Minutes:{upt[2]}\n"\
-            f"\t\t Seconds:{upt[3]}"
-        self.simStats.delete(1.0, tkinter.END)
-        self.simStats.insert(tkinter.INSERT, statsString)
-        self.simStats.pack()
-        self.master.after(1000, Sim_GUI.main)
+        statsString = ''
+        try:
+            req = requests.get(url).json()
+            fps = req['SimFPS']
+            up = req["Uptime"]
+            upt = re.split(r":|\.", up)
+            if len(upt) == 4:
+                upt.insert(0, 0)
+            agents = req['RootAg']
+            prims = req['Prims']
+            lps = req['ScrLPS']
+            (tagV, osV) = req['Version'].split('\n')
+            tagV = tagV.strip()
+            osV = osV.strip()
+            statsString = \
+                f"Sim FPS:\t\t {fps}\n"\
+                f"Visitors:\t\t {agents}\n"\
+                f"Prims: \t\t {prims}\n"\
+                f"Script LPS:\t\t {lps}\n"\
+                f"Tag Version:\t\t {tagV}\n"\
+                f"OpenSim Version:\t{osV}\n\n"\
+                f"Sim uptime:\t\t Days:{upt[0]}\n"\
+                f"\t\t Hours:{upt[1]}\n"\
+                f"\t\t Minutes:{upt[2]}\n"\
+                f"\t\t Seconds:{upt[3]}"
+        # except ConnectionResetError(args):
+        #    statsString = "Sim restarted"
+        except requests.exceptions.RequestException:
+            statsString = f"Could not connect."
+        except ValueError:
+            statsString = f"Invalid json."
+        finally:
+            self.simStats.delete(1.0, tkinter.END)
+            self.simStats.insert(tkinter.INSERT, statsString)
+            self.simStats.pack()
+            self.master.after(1000, Sim_GUI.main)
 
     def __init__(self, master, name, url):
         statsString = "Loading Stats"
