0fb8560b4bb5a7cc5da3a5d6891a51c0e9509382
Stephan Kern <stephan@swmmrman.com>
2019-12-04 05:42:25 -0700

    Clean up.

--- Count12days.py
+++ Count12days.py
@@ -5,31 +5,36 @@
 
 from time import sleep
 
-gifts = [0,0,0,0,0,0,
-			0,0,0,0,0,0];
+GIFTS = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
 
 def gift(day):
-	while day >= 1:
-		gifts[day-1] += day
-		day -=1
-		
-for day in range(0,12):
-	gift(day+1)
-	# print(day)
-total = 0;
-for i in gifts:
-	total += i
+    """Count gifts Print Text."""
+    while day >= 1:
+        GIFTS[day-1] += day
+        sleep(1)
+        day -= 1
 
-GIFTSSTRING = \
-    F"Partridges and Pear Trees:\t{gifts[0]}\n"\
-    F"Turtle Doves:\t\t{gifts[1]}\n"\
-    F"French Hens:\t\t{gifts[2]}\n"\
-    F"Calling Birds:\t\t{gifts[3]}\n"\
-    F"Golden Rings:\t\t{gifts[4]}\n"\
-    F"Geese a-laying:\t{gifts[5]}\n"\
-    F"swans a-swimming:{gifts[6]}\n"\
-    F"French Hens:{gifts[7]}\n"\
-    F"French Hens:{gifts[8]}\n"\
-    F"Total {total}"
-    
-print(GIFTSSTRING)
+
+for i in range(0, 12):
+    gift(i+1)
+
+TOTAL = 0
+for i in GIFTS:
+    TOTAL += i
+
+# GIFTSSTRING =
+
+print(
+    f"Partridges\t{GIFTS[0]}\n"
+    f"and Pear Trees:\t{GIFTS[0]}\n"
+    f"Turtle Doves:\t\t{GIFTS[1]}\n"
+    f"French Hens:\t\t{GIFTS[2]}\n"
+    f"Calling Birds:\t\t{GIFTS[3]}\n"
+    f"Golden Rings:\t\t{GIFTS[4]}\n"
+    f"Geese a-laying:\t{GIFTS[5]}\n"
+    f"swans a-swimming:{GIFTS[6]}\n"
+    f"French Hens:{GIFTS[7]}\n"
+    f"French Hens:{GIFTS[8]}\n"
+    f"Total {TOTAL}"
+)
