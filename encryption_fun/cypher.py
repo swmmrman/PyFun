a3189ded2c58087e709451399d11b5490e0c7715
Stephan Kern <stephan@swmmrman.com>
2026-04-30 17:06:10 -0600

    Add some basic functions/skelton functions

--- encryption_fun/cypher.py
+++ encryption_fun/cypher.py
@@ -1,5 +1,17 @@
 class cypher:
-    def __init__(self) -> None:
+    def __init__(self):
         self.ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ,.\"')(=:"
         self.key = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ,.\"')(=:"
         self.text = ""
+
+    def decrypt(self) -> str:
+        return self.text
+
+    def update(self, a, b):
+        self.old_key = self.key
+
+    def revert(self):
+        self.key = self.old_key
+
+    self.add_text(self, text):
+        self.text += text
