6065a28ea6fc72512e95a7089108b20bf53af9dd
Stephan Kern <stephan@swmmrman.com>
2026-04-30 21:43:28 -0600

    Add offset collection for ceaser cipher

--- encryption_fun/cypher.py
+++ encryption_fun/cypher.py
@@ -1,4 +1,6 @@
 class cypher:
+    A_Z_ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
     def __init__(self, text="", file="cyphers.txt"):
         self.ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ !,.\"')(=:?"
         self.key = "ABCDEFGHIJKLMNOPQRSTUVWXYZ !,.\"')(=:?"
@@ -8,6 +10,7 @@ class cypher:
         self.old_key = ""
         self.decrypted_text = ""
         self.load_file(file)
+        self.decrypted_offsets = []
 
     def decrypt(self) -> str:
         outstring = ""
@@ -23,11 +26,15 @@ class cypher:
 
     def update(self, a, b):
         self.old_key = self.key
+        self.decrypted_offsets.append(
+            cypher.A_Z_ALPHA.find(a.upper()) - cypher.A_Z_ALPHA.find(b.upper())
+        )
         self.key = self.key.replace(a.upper(), b)
         self.decrypted_text = self.decrypt()
 
     def revert(self):
         self.key = self.old_key
+        self.decrypted_offsets.pop(-1)
 
     def revert_letter(self, letter, one=True):
         self.old = self.key
@@ -44,3 +51,6 @@ class cypher:
         with open(file, "r") as file:
             self.text = file.read()
             self.decrypted_text = self.decrypt()
+
+
+#    def ceaser_check(self):
