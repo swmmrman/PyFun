f8deca99ee7e8c986d737f7fecc1186fce74a901
Stephan Kern <stephan@swmmrman.com>
2026-04-30 22:41:33 -0600

    Start of loop and command parsing

--- encryption_fun/interactive_decryptor.py
+++ encryption_fun/interactive_decryptor.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 import cypher
 
+
+def run_command(command, cypher: cypher.cypher):
+    parts = command.split(" ")
+    print(parts)
+
+
 print("File to read from: [ciphers.txt]")
 ans = input()
 if ans == "":
@@ -9,3 +15,9 @@ else:
     c = cypher.cypher(file=ans)
 
 c.print_decrypted()
+
+running = True
+while running:
+    print("Command:")
+    command = input()
+    run_command(command, c)
