7b0d9405b779558cf7575e2bbba6f8df37292ead
Stephan Kern <stephan@swmmrman.com>
2026-05-06 15:49:21 -0600

    more sorting

--- encryption_fun/cypher.py
+++ encryption_fun/cypher.py
@@ -16,37 +16,6 @@ class cypher:
         self.decrypted_offsets = []
         self.last_decrypted_offsets = 1
 
-    def decrypt(self) -> str:
-        outstring = ""
-        for char in self.text:
-            if char == "\n":
-                outstring += "\n"
-            elif char.upper() not in self.ALPHA:
-                outstring += char
-            else:
-                if char.isupper():
-                    outstring += self.key[self.ALPHA.find(char.upper())].upper()
-                else:
-                    outstring += self.key[self.ALPHA.find(char.upper())]
-        return outstring
-
-    def print_decrypted(self):
-        print(self.decrypted_text)
-
-    def multi_update(self, a, b):
-        length = len(a)
-        if len(b) != length:
-            print("size of strings must match")
-            return
-        self.old_key = self.key
-        self.last_decrypted_offsets = 0
-        for i in range(0, length):
-            offset = A_Z_ALPHA.find(a[i].upper()) - A_Z_ALPHA.find(b[i].upper())
-            self.decrypted_offsets.append(offset)
-            self.last_decrypted_offsets += 1
-            self.key = self.key.replace(a[i].upper(), b[i])
-            self.decrypted_text = self.decrypt()
-
     def add_text(self, text):
         self.text += text
         self.reset()
@@ -66,14 +35,13 @@ class cypher:
         self.text = in_text
         self.reset()
 
-    def clear_text(self):
-        self.text = ""
-        self.reset()
-
-    def load_file(self, file):
-        with open(file, "r") as file:
-            self.text = file.read()
-            self.decrypted_text = self.decrypt()
+    def ceaser_check(self) -> dict:
+        possible_offsets = {}
+        for i in self.decrypted_offsets:
+            occurences = self.decrypted_offsets.count(i)
+            if occurences >= 3:
+                possible_offsets.update({i: occurences})
+        return possible_offsets
 
     ### shift is the ceaser shift
     def ceaser_decrypt(self, shift) -> str:
@@ -91,13 +59,45 @@ class cypher:
                 outstring += char
         return outstring
 
-    def ceaser_check(self) -> dict:
-        possible_offsets = {}
-        for i in self.decrypted_offsets:
-            occurences = self.decrypted_offsets.count(i)
-            if occurences >= 3:
-                possible_offsets.update({i: occurences})
-        return possible_offsets
+    def clear_text(self):
+        self.text = ""
+        self.reset()
+
+    def decrypt(self) -> str:
+        outstring = ""
+        for char in self.text:
+            if char == "\n":
+                outstring += "\n"
+            elif char.upper() not in self.ALPHA:
+                outstring += char
+            else:
+                if char.isupper():
+                    outstring += self.key[self.ALPHA.find(char.upper())].upper()
+                else:
+                    outstring += self.key[self.ALPHA.find(char.upper())]
+        return outstring
+
+    def load_file(self, file):
+        with open(file, "r") as file:
+            self.text = file.read()
+            self.decrypted_text = self.decrypt()
+
+    def multi_update(self, a, b):
+        length = len(a)
+        if len(b) != length:
+            print("size of strings must match")
+            return
+        self.old_key = self.key
+        self.last_decrypted_offsets = 0
+        for i in range(0, length):
+            offset = A_Z_ALPHA.find(a[i].upper()) - A_Z_ALPHA.find(b[i].upper())
+            self.decrypted_offsets.append(offset)
+            self.last_decrypted_offsets += 1
+            self.key = self.key.replace(a[i].upper(), b[i])
+            self.decrypted_text = self.decrypt()
+
+    def print_decrypted(self):
+        print(self.decrypted_text)
 
     def reset(self):
         self.key = self.ALPHA
@@ -137,6 +137,18 @@ class cypher:
         self.decrypted_text = self.decrypt()
 
 
+def atbash_encrypt(text: str, key: str):
+    outstring = ""
+    for char in text:
+        case = char.isupper()
+        index = A_Z_ALPHA.find(char.upper())
+        new_char = key[index] if index >= 0 else char
+        if not case:
+            new_char = new_char.lower()
+        outstring += new_char
+    return outstring
+
+
 def ceaser_encrypt(text: str, offset: int):
     outstring = ""
     for char in text:
@@ -147,18 +159,6 @@ def ceaser_encrypt(text: str, offset: int):
         index = (A_Z_ALPHA.find(char.upper()) + offset) % 26
         new_char = A_Z_ALPHA[index]
         if not caps:
-            new_char = new_char.lower()
-        outstring += new_char
-    return outstring
-
-
-def atbash_encrypt(text: str, key: str):
-    outstring = ""
-    for char in text:
-        case = char.isupper()
-        index = A_Z_ALPHA.find(char.upper())
-        new_char = key[index] if index >= 0 else char
-        if not case:
             new_char = new_char.lower()
         outstring += new_char
     return outstring
